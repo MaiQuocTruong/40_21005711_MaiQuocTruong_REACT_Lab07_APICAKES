e3f6c4e241b51282528f22db11c89582deacc117
maiquoctruong <nguyenthanhtung0900@gmail.com>
2024-10-08 21:54:43 +0700

    done api cakes add to cart

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import DonutScreen from './screens/DonutScreen';
+import DetailScreen from './screens/DetailScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="DonutScreen">
+        <Stack.Screen 
+          name="DonutScreen" 
+          component={DonutScreen} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="DetailScreen" 
+          component={DetailScreen} 
+          options={{ 
+            headerTitle: '',
+            headerBackTitleVisible: false, 
+            headerStyle: {
+              elevation: 0, 
+              shadowOpacity: 0, 
+              borderBottomWidth: 0, 
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
